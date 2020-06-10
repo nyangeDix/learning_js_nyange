cceb829ff8b0e2a88d0bfb7180d4c200937332eb
Dickson <nyangedickson@gmail.com>
2020-06-10 23:43:14 +0300

    adding dynamic cube change

--- dom_manipulation/app.js
+++ dom_manipulation/app.js
@@ -9,21 +9,37 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScores = 0;
 activePlayer = 1;
-dice = Math.random()
-
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var y = document.querySelector('#score-1').textContent;
-
-//alert(y);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+
+    diceDOM.style.display = 'block';
+
+    diceDOM.src = 'dice-' + dice + '.png';  
+
+});
+
+
+//console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var y = document.querySelector('#score-1').textContent;
+
+//alert(y);
