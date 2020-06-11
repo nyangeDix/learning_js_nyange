7b738a9e5daf9f59595f71de5153a6ce9d5ddc1b
Dickson <nyangedickson@gmail.com>
2020-06-11 22:49:53 +0300

    adding app.js - almost done with game

--- dom_manipulation/app.js
+++ dom_manipulation/app.js
@@ -9,7 +9,9 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScores, activePlayer, gamePlaying;
+
+init();
 
 scores = [0,0];
 roundScores = 0;
@@ -47,7 +49,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     //check if player won the game
    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-   nextPlayer();
+    if(scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
+    }
+
+   
 
 });
 
@@ -63,6 +74,28 @@ function nextPlayer() {
 
         document.querySelector('.dice').style.display = 'none';
 } 
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
 //console.log(dice);
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
