bce9ee887e65865f358f78c429efd7ae94c29409
Dickson <nyangedickson@gmail.com>
2020-06-23 23:49:00 +0300

    done implementing UI controllers and Initialization function

--- appBudget/app.js
+++ appBudget/app.js
@@ -4,22 +4,59 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn'
+    }
+
+    return {
+        getInput : function() {
+            return {
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings : function() {
+            return DOMstrings;
+        }
+    };
+
+    
+
 })();
 
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var ctrlAddItem = function() {
+    var setUpEventListeners = function() {
 
+        var DOM = UIController.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function() {
+
+            if(event.keycode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+
+        });
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    var ctrlAddItem = function() {
+        var input = UIctrl.getInput();
+        console.log(input);
+    }
 
-    document.addEventListener('keypress', function() {
-
-        if(event.keycode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            setUpEventListeners();
         }
-
-    });
+    }
 
 })(budgetController, UIController);
+
+controller.init();
