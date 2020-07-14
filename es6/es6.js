6f6fe347e76e6ea4a21994bfff04e7588eaae626
Dickson <nyangedickson@gmail.com>
2020-07-14 22:57:49 +0300

    lexical keyword this work in es6

--- es6/es6.js
+++ es6/es6.js
@@ -37,3 +37,33 @@ let newAges = yearofBirth.map(el => 2020 - el);
 
 console.log('New ages.....')
 console.log(newAges);
+
+const box1 = {
+    color : 'purple',
+    position : 1,
+    clickMe : function() {
+
+        document.querySelector('.btn').addEventListener('click', () => {
+            var str = `This is box number ${this.position} of ${this.color}`;
+            alert(str);
+        });
+
+    }
+}
+
+box1.clickMe();
+
+console.log('Let\'s print the second statement');
+
+var friends = ['Dixon', 'XCode', 'Shadow Brokers', 'Pennywise'];
+
+function Person (name) {
+    this.name = name;
+}
+
+Person.prototype.myfriends = (friends => {
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+});
+
+new Person('Mike').myfriends(friends);
